2e801816666d1ac514b9f68007b95ac25320a3cb
Black Cat <yekdoust@gmail.com>
2023-11-13 04:59:38 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+bool isprime = true;
+Console.WriteLine("prime number:");
+for (int i = 2; i < 1000; i++)
+{
+    for (int j = 2; j <= 1000; j++)
+    {
+        if (i != j && i % j == 0)
+        {
+            isprime = false;
+            break;
+        }
+    }
+    if (isprime)
+    {
+        Console.Write("\t" + i);
+    }
+    isprime = true;
+}
+Console.ReadKey();
